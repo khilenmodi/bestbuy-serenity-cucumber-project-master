563b6c585c5f5d3b06a271209a3da9b75f1adfc8
khilenmodi <maharajswami007@gmail.com>
2023-08-22 23:17:00 +0100

    homework cucumber project

---
+++ bestbuy-serenity-cucumber-project-master/src/test/java/com/bestbuy/testbase/TestBase.java
@@ -0,0 +1,22 @@
+package com.bestbuy.testbase;
+
+import com.bestbuy.utils.PropertyReader;
+import com.bestbuy.utils.TestUtils;
+import io.restassured.RestAssured;
+import org.junit.BeforeClass;
+
+/**
+ * Created by Jay Vaghani
+ */
+public class TestBase extends TestUtils {
+    public static PropertyReader propertyReader;
+    @BeforeClass
+    public static void init() {
+        propertyReader = PropertyReader.getInstance();
+
+        RestAssured.baseURI = propertyReader.getProperty("baseUrl");
+        RestAssured.port = Integer.parseInt(propertyReader.getProperty("port"));
+
+    }
+
+}
